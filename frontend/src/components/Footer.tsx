4734b9075f89da7c74dc510713b063bc3deb449a
Dayanch437 <dayanchkemalovich@gmail.com>
2025-03-20 12:24:10 +0500

    logo instagram and tiktok added

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
-import { FaShoppingCart } from 'react-icons/fa';
+import { FaShoppingCart } from "react-icons/fa";
+import { FaInstagram, FaTiktok } from "react-icons/fa";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4 px-4 md:px-16 lg:px-24 ">
+    <footer className="bg-gray-800 text-white py-4 px-4 md:px-16 lg:px-24">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
-        {/* Logo and Text */}
-        <div className="mb-4 md:mb-0 flex items-center"> {/* Flexbox ile yan yana */}
-          <FaShoppingCart className="h-12 w-12 text-white mr-2" /> {/* marginRight ile boşluk */}
-          <span className="text-lg font-semibold">Gyrat</span> {/* Yazı */}
+        {/* Social Media Links */}
+        <div className="flex space-x-4 mb-4 md:mb-0">
+          <a href="https://www.instagram.com/gyrat_" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="h-6 w-6 text-white hover:text-gray-400" />
+          </a>
+          <a href="https://www.tiktok.com/@gyrat_" target="_blank" rel="noopener noreferrer">
+            <FaTiktok className="h-6 w-6 text-white hover:text-gray-400" />
+          </a>
         </div>
 
-        {/* Address */}
+        {/* Logo and Text */}
+        <div className="mb-4 md:mb-0 flex items-center">
+          <FaShoppingCart className="h-12 w-12 text-white mr-2" />
+          <span className="text-lg font-semibold">Market</span>
+        </div>
+
+        {/* Phone Number */}
         <div className="text-center md:text-right">
-          <p>123 Example St, Example City, Postal Code</p>
+          <a href="tel:+99361983821" className="text-white hover:text-gray-400">+99361983821</a>
         </div>
       </div>
-      {/* Copyright Section (Optional) */}
+
+      {/* Copyright Section */}
       <div className="mt-4 text-center border-t border-gray-700 pt-2 text-sm">
         <p>&copy; {new Date().getFullYear()} All Rights Reserved.</p>
       </div>
